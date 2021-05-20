7809dc58c2f2fc348f5ba0b5b4e03daebea63fba
Loi Fuh Chang <fuh_chang@Hotmail.com>
2021-05-20 13:02:50 +0800

    First commit for validator struct practice

---
+++ FcValidator/FcValidator/FcValidator.h
@@ -0,0 +1,18 @@
+//
+//  FcValidator.h
+//  FcValidator
+//
+//  Created by Fuh chang Loi on 20/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FcValidator.
+FOUNDATION_EXPORT double FcValidatorVersionNumber;
+
+//! Project version string for FcValidator.
+FOUNDATION_EXPORT const unsigned char FcValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FcValidator/PublicHeader.h>
+
+
